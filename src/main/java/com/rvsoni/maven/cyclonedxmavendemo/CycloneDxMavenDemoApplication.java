1cc15975591963e8763a6dc9858faef984f8001d
Ravi Soni <ravi.soni@infodesk.com>
2022-06-23 22:48:40 +0530

    Added CycloneDX Maven plugin

---
+++ src/main/java/com/rvsoni/maven/cyclonedxmavendemo/CycloneDxMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rvsoni.maven.cyclonedxmavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycloneDxMavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycloneDxMavenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rvsoni/maven/cyclonedxmavendemo/CycloneDxMavenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvsoni.maven.cyclonedxmavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycloneDxMavenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
